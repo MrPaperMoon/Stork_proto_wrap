a80fcfcc3d65d68435a7ed56e6fe3faf785df3b5
Vyacheslav Blinov <blinov.vyacheslav@gmail.com>
2021-06-26 16:56:57 +0300

    kotlin -> 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `java-library`
     idea
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.10"
     id("com.google.protobuf") version "0.8.13"
 }
 
