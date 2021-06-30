5bca352e19ce175be2f3a0cd59260ee7c71cfa35
Vyacheslav Blinov <blinov.vyacheslav@gmail.com>
2021-06-30 10:54:33 +0300

    more event streams

--- src/main/kotlin/io/stork/client/module/EventWebsocket.kt
+++ src/main/kotlin/io/stork/client/module/EventWebsocket.kt
@@ -2,6 +2,12 @@ package io.stork.client.module
 
 import io.stork.proto.calls.conference.ConferenceEvent
 import io.stork.proto.calls.rtc.RTCEvent
+import io.stork.proto.chat.ChatEvent
+import io.stork.proto.client.recordings.recording.RecordingEvent
+import io.stork.proto.client.workspace.WorkspaceEvent
+import io.stork.proto.files.file.FileEvent
+import io.stork.proto.member.MemberEvent
+import io.stork.proto.profile.ProfileEvent
 import io.stork.proto.websocket.EchoMessage
 import io.stork.proto.websocket.WebsocketEvent
 import kotlinx.coroutines.flow.Flow
@@ -19,6 +25,36 @@ interface EventWebsocket {
     val conferenceEvents: Flow<ConferenceEvent>
         get() = allEvents.getEvents(WebsocketEvent.EventCase.CONFERENCE_EVENT) {
             it.conferenceEvent
+        }
+
+    val memberEvents: Flow<MemberEvent>
+        get() = allEvents.getEvents(WebsocketEvent.EventCase.MEMBER_EVENT) {
+            it.memberEvent
+        }
+
+    val profileEvents: Flow<ProfileEvent>
+        get() = allEvents.getEvents(WebsocketEvent.EventCase.PROFILE_EVENT) {
+            it.profileEvent
+        }
+
+    val workspaceEvents: Flow<WorkspaceEvent>
+        get() = allEvents.getEvents(WebsocketEvent.EventCase.WORKSPACE_EVENT) {
+            it.workspaceEvent
+        }
+
+    val recordingEvents: Flow<RecordingEvent>
+        get() = allEvents.getEvents(WebsocketEvent.EventCase.RECORDING_EVENT) {
+            it.recordingEvent
+        }
+
+    val chatEvents: Flow<ChatEvent>
+        get() = allEvents.getEvents(WebsocketEvent.EventCase.CHAT_EVENT) {
+            it.chatEvent
+        }
+
+    val fileEvents: Flow<FileEvent>
+        get() = allEvents.getEvents(WebsocketEvent.EventCase.FILE_EVENT) {
+            it.fileEvent
         }
 }
 
