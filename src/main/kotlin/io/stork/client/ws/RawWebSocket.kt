c75efad470ddc0472493e4f60a01bdaf052ffa25
Vyacheslav Blinov <blinov.vyacheslav@gmail.com>
2021-07-20 15:12:09 +0300

    Rename io.stork.client.ws.WebSocket to RawWebSocket

--- src/main/kotlin/io/stork/client/ws/RawWebSocket.kt
+++ src/main/kotlin/io/stork/client/ws/RawWebSocket.kt
@@ -4,7 +4,7 @@ import io.stork.proto.websocket.ClientWSPacket
 import io.stork.proto.websocket.ServerWSPacket
 import kotlinx.coroutines.flow.Flow
 
-interface WebSocket {
+interface RawWebSocket {
     val received: Flow<ServerWSPacket>
     suspend fun send(payload: ClientWSPacket)
     suspend fun close()
