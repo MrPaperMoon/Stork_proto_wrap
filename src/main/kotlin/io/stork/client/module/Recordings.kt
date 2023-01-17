aab7a4f2a8dd3b6ac0d36c28dfd586de665c5fa9
kamal <kamal@stork.ai>
2023-01-17 14:00:24 +0800

    ANDROID-176: Display transcriptions in call recordings - add mark remove recording call

--- src/main/kotlin/io/stork/client/module/Recordings.kt
+++ src/main/kotlin/io/stork/client/module/Recordings.kt
@@ -3,10 +3,13 @@ package io.stork.client.module
 import io.stork.client.ApiResult
 import io.stork.proto.client.recording.RecordingListRequest
 import io.stork.proto.client.recording.RecordingListResponse
+import io.stork.proto.client.recording.RemoveRecordingRequest
+import io.stork.proto.client.recording.RemoveRecordingResponse
 import io.stork.proto.client.recording.UpdateRecordingTitleRequest
 import io.stork.proto.client.recording.UpdateRecordingTitleResponse
 
 interface Recordings {
     suspend fun list(body: RecordingListRequest): ApiResult<RecordingListResponse>
     suspend fun updateTitle(body: UpdateRecordingTitleRequest): ApiResult<UpdateRecordingTitleResponse>
+    suspend fun remove(body: RemoveRecordingRequest): ApiResult<RemoveRecordingResponse>
 }
